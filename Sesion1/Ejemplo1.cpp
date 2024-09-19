8bf9f9ec8690ddaba6abcbd50d7395612623dae1
lorenaUAH <lorena.batanero@edu.uah.es>
2024-09-19 23:18:20 +0200

    Ejercicios-Sesion1

---
+++ Sesion1/Ejemplo1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int x;
+	std::cout << "Introduzca un número: ";
+	std::cin >> x;
+	std::cout << "El número es: " << x <<"\n";
+	
+	return 0;
+}
+
+
